01fc94e36f00d957baf6c4ca9aa42ddf93895da2
Joe Rohd <joerohd@gmail.com>
2016-04-30 07:10:08 -0700

    Time unit tests, part 18

--- include/Time.hpp
+++ include/Time.hpp
@@ -16,10 +16,11 @@ namespace Camax
     public:
         Time(int hours = 0, int minutes = 0, int seconds = 0);
         int getTotalTimeAsSeconds() const;
-        const int secondsInAnHour = 3600;
-        const int secondsInAMinute = 60;
         const Time& operator+=(const Time& rhs);
         friend ostream& operator<<(ostream& os, const Time& time);
+
+        const int secondsInAnHour = 3600;
+        const int secondsInAMinute = 60;
 
     private:
         int seconds;
