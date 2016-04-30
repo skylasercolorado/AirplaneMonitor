b00e0d76086bf07694560bd38789d64339d20dbd
Joe Rohd <joerohd@gmail.com>
2016-04-30 16:55:07 -0700

    Signal, part 2

--- tests/SignalTests.cpp
+++ tests/SignalTests.cpp
@@ -48,6 +48,8 @@ TEST_F(SignalTests, Create)
 {
     Time b(0,0,45);
     TestSignal a(23, b);
+    Time temp = a.getTime();
 
     EXPECT_EQ(23, a.getVoltage());
+    EXPECT_EQ(45, temp.getTotalTimeAsSeconds());
 }
