6f45ba97b573eea916214c1018840d95b42f39d7
Joe Rohd <joerohd@gmail.com>
2016-04-29 21:43:58 -0700

    Time unit tests, part 4

--- tests/TimeTests.cpp
+++ tests/TimeTests.cpp
@@ -22,9 +22,62 @@ public:
     }
 };
 
+struct timeParams
+{
+    int hour;
+    int minute;
+    int seconds;
+
+    timeParams(int hour, int minute, int seconds)
+    {
+        this->hour = hour;
+        this->minute = minute;
+        this->seconds = seconds;
+    }
+};
+
+class TimeParams : public TimeTests, public ::testing::WithParamInterface<timeParams>
+{
+public:
+    void SetUp()
+    {
+
+    }
+
+    void TearDown()
+    {
+
+    }
+
+private:
+
+};
+
 TEST_F(TimeTests, Create)
 {
     Time time(0, 0, 1);
 
     EXPECT_EQ(1, time.getTotalTimeAsSeconds());
 }
+
+TEST_P(TimeParams, valueSweep)
+{
+    timeParams params = GetParam();
+
+    Time time(params.hour, params.minute, params.seconds);
+    EXPECT_EQ(params.hour * 3600 + params.minute * 60 + params.seconds, time.getTotalTimeAsSeconds());
+}
+
+std::vector<timeParams> generateParams()
+{
+    std::vector<timeParams> params;
+    for(int i = 0; i <= 23; i+=10)
+        for(int j = 0; j <= 59; j+=10)
+            for(int k = 0; k <= 59; k+=10)
+                params.push_back(* new timeParams(i, j, k));
+
+    return params;
+}
+
+INSTANTIATE_TEST_CASE_P(timeParamsSweep, TimeParams, ::testing::ValuesIn(generateParams()));
+
