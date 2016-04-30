87b3b58d56d50e6b88af915f36bf505ac16c4025
Joe Rohd <joerohd@gmail.com>
2016-04-30 16:50:06 -0700

    Signal, part 1

---
+++ include/Signal.hpp
@@ -0,0 +1,31 @@
+//
+// Created by duncan on 4/30/16.
+//
+
+#ifndef AIRPLANEMONITOR_SIGNAL_HPP
+#define AIRPLANEMONITOR_SIGNAL_HPP
+
+#include "Time.hpp"
+
+namespace Camax
+{
+    class Signal
+    {
+    public:
+        Signal(double valueOffset, Time timeOffset)
+        {
+            this->voltageOffset = valueOffset;
+            this->timeOffset = timeOffset;
+        }
+        virtual double getVoltageAtTime(Time t) = 0;
+
+    protected:
+        double getVoltageOffset() { return voltageOffset; }
+        Time getTimeOffset() { return timeOffset; }
+
+    private:
+        double voltageOffset;
+        Time timeOffset;
+    };
+}
+#endif //AIRPLANEMONITOR_SIGNAL_HPP
---
+++ tests/SignalTests.cpp
@@ -0,0 +1,53 @@
+//
+// Created by duncan on 4/30/16.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "Signal.hpp"
+
+using namespace Camax;
+
+class TestSignal : public Signal
+{
+public:
+    TestSignal(double voltage, Time timeOffset) : Signal(voltage, timeOffset) {}
+
+    virtual double getVoltageAtTime(Time t)
+    {
+        return 3.1415;
+    }
+
+    double getVoltage()
+    {
+        return this->getVoltageOffset();
+    }
+
+    Time getTime()
+    {
+        return this->getTimeOffset();
+    }
+
+};
+
+class SignalTests : public ::testing::Test
+{
+public:
+    void SetUp()
+    {
+
+    }
+
+    void TearDown()
+    {
+
+    }
+};
+
+TEST_F(SignalTests, Create)
+{
+    Time b(0,0,45);
+    TestSignal a(23, b);
+
+    EXPECT_EQ(23, a.getVoltage());
+}
