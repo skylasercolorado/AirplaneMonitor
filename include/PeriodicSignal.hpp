036089b0a7954c248e746aef9e9bda7175938531
Joe Rohd <joerohd@gmail.com>
2016-04-30 21:44:07 -0700

    periodic signal, part 1

---
+++ include/PeriodicSignal.hpp
@@ -0,0 +1,33 @@
+//
+// Created by duncan on 4/30/16.
+//
+
+#ifndef AIRPLANEMONITOR_PERIODICSIGNAL_HPP
+#define AIRPLANEMONITOR_PERIODICSIGNAL_HPP
+
+#include "Signal.hpp"
+
+using namespace Camax;
+
+class PeriodicSignal : public Signal
+{
+public:
+    PeriodicSignal(double voltageOffset, Time timeOffset, double minVoltage, double maxVoltage, Time period) :
+            Signal(voltageOffset, timeOffset), minValue(minVoltage), maxValue(maxVoltage)
+    {
+       this->period(period);
+    }
+    virtual double getVoltageAtTime(Time t) = 0;
+
+protected:
+    double getMinVoltage() { return minValue; };
+    double getMaxVoltage() { return maxValue; };
+    Time getPeriod() { return period; };
+
+private:
+    double minValue;
+    double maxValue;
+    Time period;
+};
+
+#endif //AIRPLANEMONITOR_PERIODICSIGNAL_HPP
