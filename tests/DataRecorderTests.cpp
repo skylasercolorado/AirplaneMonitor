f4afb7a9ec530787ee6daa1bb6211b0735667350
Joe Rohd <joerohd@gmail.com>
2016-05-05 12:44:59 -0700

    data recorder tests, part 2

---
+++ tests/DataRecorderTests.cpp
@@ -0,0 +1,22 @@
+//
+// Created by duncan on 5/5/16.
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "DataRecorder.hpp"
+
+using namespace Camax;
+
+class DataRecorderTests : public ::testing::Test
+{
+    virtual void SetUp() {}
+
+    virtual void TearDown() {}
+};
+
+TEST_F(DataRecorderTests, Construct)
+{
+
+}
