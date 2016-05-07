387ddcf7867181195ff861d35ea2facd6b6ea2e6
Joe Rohd <joerohd@gmail.com>
2016-05-07 13:41:04 -0700

    data recorder tests, part 15

--- tests/MonitoringSystemTests.cpp
+++ tests/MonitoringSystemTests.cpp
@@ -10,27 +10,42 @@
 
 using namespace Camax;
 
+
 class MonitoringSystemTests : public ::testing::Test
 {
+public:
     void SetUp() {}
 
     void TearDown() {}
+
+private:
 };
+
+string verificationString(Time timeStamp, string sensorName, double sensorVoltage, string sensorUnits)
+{
+    stringbuf buffer;
+    ostream os(&buffer);
+
+    os << timeStamp << ", sensor: " << sensorName << ", measurement: " << sensorVoltage << " (" << sensorUnits << ")\n";
+
+    return buffer.str();
+}
 
 TEST_F(MonitoringSystemTests, ConstructAndUseOneSensor)
 {
     stringbuf buffer;
     ostream os(&buffer);
-
     MonitoringSystem monitor(os);
 
     double voltage = 23.45;
     Time time1(0, 10, 10);
     Signal *signal = new ConstantSignal(voltage, time1);
-
     Sensor *sensor = new AngularSensor("Direction", *signal);
 
     monitor.AddSensor(sensor);
     monitor.TakeReading(time1);
 
+    string checkString = verificationString(time1, "Direction", voltage, "radians");
+    EXPECT_EQ(checkString, buffer.str());
 }
+
